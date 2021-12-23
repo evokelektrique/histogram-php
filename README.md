4a012c512b3aca7852ab474791dab2512872d184
EVOKE <evoke.lektrique@gmail.com>
2021-12-23 22:29:46 +0330

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Historgram PHP
+<div align="center">
+  <b>Historgram PHP</b>
+  <br>
+  <p>A histogram is an approximate representation of the distribution of numerical data. It was first introduced by Karl Pearson. <a href="https://en.wikipedia.org/wiki/Histogram">Wikipedia<a/></p>
+</div>
+
+## Usage
+
+```php
+// Load a source into model
+$file = "./temp/desert.jpg";
+$histogram = new Histogram($file);
+
+// Custom styles
+$options = [
+  // Bars max height
+  "max_height" => 256,
+
+  // Bars styles
+  "bar_styles" => [
+    "width" => "4px",
+    "background" => "linear-gradient(to bottom, #2380ac, #2f2629)",
+    "display" => "inline-block",
+    "border-top-right-radius" => "2px",
+    "border-top-left-radius" => "2px",
+  ],
+
+  // Wrapper styles
+  "wrapper_styles" => [
+    "display" => "inline-table",
+  ]
+];
+
+// Display histogram model
+$histogram->display($options);
+```
+And if you want to get a list of bar heights for more customization you could use the below example:
+
+```php
+// Get a list of bars from the model with maximum heights of "300"
+$bars = $histogram->get_bars($histogram->histogram, 300);
+```
--- histogram.php
+++ histogram.php
@@ -1,56 +1,131 @@
 <?php
-// Histogram PHP
 
-// Image inputs
-$sources = ["./temp/test1.png", "./temp/test.jpg"];
+/**
+ * Histogram PHP base class
+ */
+final class Histogram {
 
-// Loop through sources
-foreach ($sources as $source) {
-  $image = null;
+  public $histogram;
 
-  // Create PNG image
-  if(exif_imagetype($source) === IMAGETYPE_PNG) {
-    $image = imagecreatefrompng($source);
+  public function __construct($source) {
+    $image = null;
+
+    // Create PNG image
+    if(exif_imagetype($source) === IMAGETYPE_PNG) {
+      $image = imagecreatefrompng($source);
+    }
+
+    // Create JPG image
+    if(exif_imagetype($source) === IMAGETYPE_JPEG) {
+      $image = imagecreatefromjpeg($source);
+    }
+
+    if(!$image) {
+      return;
+    }
+
+    $this->histogram = $this->gram($image);
   }
 
-  // Create JPG image
-  if(exif_imagetype($source) === IMAGETYPE_JPEG) {
-    $image = imagecreatefromjpeg($source);
+  /**
+   * Display histogram model
+   *
+   * @return void
+   */
+  public function display(array $options): void {
+    if(empty($this->histogram) || empty($options)) {
+      return;
+    }
+
+    // Format styles
+    $bar_styles = $this->format_styles($options["bar_styles"]);
+    $wrapper_styles = $this->format_styles($options["wrapper_styles"]);
+
+    echo "<div style='$wrapper_styles'>";
+    // Loop through bars and display them with their styles
+    foreach($this->get_bars($this->histogram, $options["max_height"]) as $bar_height) {
+      echo "<span style='height:$bar_height;$bar_styles'></span>";
+    }
+    echo "</div>";
   }
 
-  if(!$image) {
-    return;
-  }
+  /**
+   * Generate histogram form given source
+   *
+   * @param  resource $image
+   * @return array
+   */
+  public function gram($image): array {
+    // Get image dimensions
+    $image_width = imagesx($image);
+    $image_height = imagesy($image);
+    $total_pixels = $image_width * $image_height;
 
-  $image_width = imagesx($image);
-  $image_height = imagesy($image);
-  $total_pixels = $image_width * $image_height;
+    $histogram = [];
 
-  print_r(histogram($image, $image_width, $image_height, $total_pixels));
-}
+    for ($i=0; $i < $image_width; $i++) {
+      for ($j=0; $j < $image_height; $j++) {
+        // Get RGB colors of current pixel
+        $rgb = imagecolorat($image, $i, $j);
+        $r = ($rgb >> 16) & 0xFF;
+        $g = ($rgb >> 8) & 0xFF;
+        $b = $rgb & 0xFF;
 
-function histogram($image, int $image_width, int $image_height, int $total_pixels): array {
-  $histogram = [];
+        $histogram_v = (int) round(($r + $g + $b) / 3);
 
-  for ($i=0; $i < $image_width; $i++) {
-    for ($j=0; $j < $image_height; $j++) {
-      $rgb = imagecolorat($image, $i, $j);
-      $r = ($rgb >> 16) & 0xFF;
-      $g = ($rgb >> 8) & 0xFF;
-      $b = $rgb & 0xFF;
-
-      $histogram_v = (int)round(($r + $g + $b) / 3);
-      $value = $histogram_v/$total_pixels;
-      // echo "histogram_v($histogram_v) / total_pixels($total_pixels) = {$value}\n";
-
-      if(array_key_exists($histogram_v, $histogram)) {
-        $histogram[$histogram_v] += $value;
-      } else {
-        $histogram[$histogram_v] = $value;
+        if(array_key_exists($histogram_v, $histogram)) {
+          $histogram[$histogram_v] += $histogram_v/$total_pixels;
+        } else {
+          $histogram[$histogram_v] = $histogram_v/$total_pixels;
+        }
       }
     }
+
+    arsort($histogram); // Sort the model
+    return $histogram;
   }
 
-  arsort($histogram);
-  return $histogram;
+  /**
+   * Get a list of bar heights
+   *
+   * @param  array       $histogram  gram model
+   * @param  int|integer $max_height Maximum bars height
+   * @return array
+   */
+  public function get_bars(array $histogram, int $max_height): array {
+    $bars = [];
+    $max = 0; // Max value in gram
+    $val = 0;
+    for ($i=0; $i<255; $i++) {
+      if (array_key_exists($i, $histogram) && $histogram[$i] > $max) {
+        $max = $histogram[$i];
+      }
+    }
+
+    for ($i=0; $i<255; $i++) {
+      if (array_key_exists($i, $histogram)) {
+        $val += $histogram[$i];
+        $h = ( $histogram[$i] / $max ) * $max_height;
+        $bars[] = $h;
+      }
+    }
+
+    return $bars;
+  }
+
+  /**
+   * Generate formatted CSS styles from an array
+   *
+   * @param  array  $styles_array styles array
+   * @return string               CSS styles
+   */
+  private function format_styles(array $styles_array): string {
+    $styles = "";
+
+    foreach ($styles_array as $key => $value) {
+      $styles .= "$key:$value;";
+    }
+
+    return $styles;
+  }
 }
